999e1f7512b66d119749997d55c47a1ac0d75640
Md. Rahat <rahat.cse5.bu@gmail.com>
2025-02-24 19:11:55 +0600

    borjoniyoo fixed

--- lib/modules/borjoniyo/view/borjoniyo_view.dart
+++ lib/modules/borjoniyo/view/borjoniyo_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:ramadan_tracker/app/common/widgets/custom_appbar_widget.dart';
 import '../controller/borjoniyo_controller.dart';
 
 class BorjoniyoView extends GetView<BorjoniyoController> {
@@ -7,20 +8,26 @@ class BorjoniyoView extends GetView<BorjoniyoController> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: ListView.builder(itemCount:controller.borjoniyoList.length , itemBuilder: (context, index) {
-      String borjoniyoText = controller.borjoniyoList[index].text;
-
-      return Card(
-        elevation: 3.5,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(6),
-        ),
-        child: ListTile(
-          leading: Icon(Icons.close),
-          title: Text(
-              borjoniyoText), // Assuming 'text' is the key for the text you want to display
-        ),
-      );
-    }));
+    return Scaffold(
+      appBar: CustomAppBar.appBar(title: ''),
+      body: Center(child: Align(
+alignment: Alignment.center,
+        child: ListView.builder(itemCount:controller.borjoniyoList.length , itemBuilder: (context, index) {
+          String borjoniyoText = controller.borjoniyoList[index].text;
+        
+          return Card(
+            elevation: 3.5,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(6),
+            ),
+            child: ListTile(
+              leading: Icon(Icons.close),
+              title: Text(
+                  borjoniyoText), // Assuming 'text' is the key for the text you want to display
+            ),
+          );
+        }),
+      )),
+    );
   }
 }
