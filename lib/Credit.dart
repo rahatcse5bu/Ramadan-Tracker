01029e9616f33ba1b334b1b5420c5452fac937af
Md. Rahat <rahat.cse5.bu@gmail.com>
2025-03-02 00:00:18 +0600

    Credit modified

--- lib/Credit.dart
+++ lib/Credit.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'colors.dart';
 
@@ -14,24 +15,64 @@ class _CreditState extends State<Credit> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-             SizedBox(height: 8),
-               Container(
-                width:  double.infinity,  
-                color: AppColors.PrimaryColor.withOpacity(0.2),
-                child: Column(
-                 children: [
-                  SizedBox(height: 12,),
-                   Text("Developed By:",style: TextStyle(color:AppColors.PrimaryColor,fontSize: 20),),
-                   SizedBox(height: 6,),
-                   Text("PNC Soft Tech",style: TextStyle(color:AppColors.PrimaryColor,fontSize: 20),),
-                   SizedBox(height: 6,),
-                   Text("Website: https://pncsoft.tech",style: TextStyle(color:AppColors.PrimaryColor,fontSize: 20),),
-                  SizedBox(height: 6,),
-                   Text("Whatsapp: +8801793278360",style: TextStyle(color:AppColors.PrimaryColor,fontSize: 20),),
-                   SizedBox(height: 12,),
-                 ],
-               )),
-              SizedBox(height: 8),
+        SizedBox(height: 8),
+        Container(
+            width: double.infinity,
+            color: AppColors.PrimaryColor.withOpacity(0.026),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 12,
+                ),
+                Text(
+                  "Developed By:",
+                  style: TextStyle(color: AppColors.PrimaryColor, fontSize: 20),
+                ),
+                SizedBox(
+                  height: 6,
+                ),
+                GestureDetector(
+                  onTap: () async {
+                    await launchUrl(Uri.parse('https://pncsoft.tech'));
+                  },
+                  child: Text(
+                    "PNC Soft Tech",
+                    style:
+                        TextStyle(color: AppColors.PrimaryColor, fontSize: 20),
+                  ),
+                ),
+                SizedBox(
+                  height: 6,
+                ),
+                GestureDetector(
+                  onTap: () async {
+                    await launchUrl(Uri.parse('https://pncsoft.tech'));
+                  },
+                  child: Text(
+                    "Website: https://pncsoft.tech",
+                    style:
+                        TextStyle(color: AppColors.PrimaryColor, fontSize: 20),
+                  ),
+                ),
+                SizedBox(
+                  height: 6,
+                ),
+                GestureDetector(
+                  onTap: () async {
+                    await launchUrl(Uri.parse('https://wa.me/+8801793278360'));
+                  },
+                  child: Text(
+                    "Whatsapp: +8801793278360",
+                    style:
+                        TextStyle(color: AppColors.PrimaryColor, fontSize: 20),
+                  ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+              ],
+            )),
+        SizedBox(height: 8),
       ],
     );
   }
