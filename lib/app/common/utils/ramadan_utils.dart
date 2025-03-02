609189f2c22594721bfc97812b59f2199964135a
Md. Rahat <rahat.cse5.bu@gmail.com>
2025-03-02 06:33:37 +0600

    getNumberSuffix utils function added

--- lib/app/common/utils/ramadan_utils.dart
+++ lib/app/common/utils/ramadan_utils.dart
@@ -163,4 +163,21 @@ class Utils {
 static Future<String?> getUserName() async {
   return StorageHelper.getUserName();
 }
+static String getNumberSuffix(int number) {
+  if (number >= 11 && number <= 13) {
+    return 'th'; // Special case for 11th, 12th, 13th
+  }
+  
+  switch (number % 10) {
+    case 1:
+      return 'st';
+    case 2:
+      return 'nd';
+    case 3:
+      return 'rd';
+    default:
+      return 'th';
+  }
+}
+
 }
